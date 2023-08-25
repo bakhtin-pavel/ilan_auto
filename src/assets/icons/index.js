9b132bc2629beeed0c1e7f35cb7d77e4941163f2
bakhtin_pavel <p1avel.com@gmial.com>
2023-08-25 11:34:31 +0700

    test popAutoSlider 1.2(fix error)

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,6 +2,5 @@ import { ReactComponent as LogoImg } from './logo.svg';
 import { ReactComponent as Whatsapp } from './whatsapp.svg';
 import { ReactComponent as Telegram } from './telegram.svg';
 import { ReactComponent as VK } from './vk.svg';
-import { ReactComponent as SlideNextIcon } from './slide_next_button.svg';
 
-export { LogoImg, Whatsapp, Telegram, VK, SlideNextIcon };
+export { LogoImg, Whatsapp, Telegram, VK };
